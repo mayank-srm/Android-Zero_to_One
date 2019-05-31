af440b9ecf819392b14546f4123a8e0b9763ec67
MAYANK SINGH <mayu198@users.noreply.github.com>
2019-05-31 18:30:28 +0530

    day_2 done

--- app/src/main/java/com/mayank/androidbasics/BasicsActivity/BasicsActivities.java
+++ app/src/main/java/com/mayank/androidbasics/BasicsActivity/BasicsActivities.java
@@ -12,29 +12,8 @@ public class BasicsActivities extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basics_activities);
-
-    //    loadFragment(new HomeFragment());
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-//        navView.setOnNavigationItemSelectedListener(this);
-
         getIncomingIntent();
     }
-//        @SuppressLint("SetTextI18n")
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            Fragment fragment = null;
-//            switch (item.getItemId()) {
-//                case R.id.navigation_home:
-//                    fragment = new HomeFragment();
-//                    break;
-//                case R.id.navigation_dashboard:
-//                    fragment = new XmlFragment();
-//                    break;
-//                case R.id.navigation_notifications:
-//                    fragment = new HomeFragment();
-//                    break;
-//
-//        }
 
 
     private void getIncomingIntent(){
@@ -54,7 +33,6 @@ public class BasicsActivities extends AppCompatActivity{
 
         TextView actname  = findViewById(R.id.message1);
         TextView actdis  = findViewById(R.id.message2);
-
         TextView javacode  = findViewById(R.id.message3);
         TextView xmlcode  = findViewById(R.id.message4);
 
